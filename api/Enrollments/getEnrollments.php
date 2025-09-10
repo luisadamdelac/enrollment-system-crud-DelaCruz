8e5830b1ccfa5b6c8e806b9c8db56e8c535500c7
Dela Cruz, Luis Adam D. <luisadamdelacruz8@gmail.com>
2025-09-10 10:21:29 +0800

    Complete Student Management System

--- api/Enrollments/getEnrollments.php
+++ api/Enrollments/getEnrollments.php
@@ -3,7 +3,6 @@ header('Content-Type: application/json');
 require '../db.php';
 
 try {
-    // Return all enrollments with student and subject info
     $stmt = $pdo->query("SELECT sl.load_id, sl.stud_id, sl.subject_id, s.name AS student_name, sub.subject_name FROM student_load sl JOIN student_tbl s ON sl.stud_id = s.stud_id JOIN subject_tbl sub ON sl.subject_id = sub.subject_id");
     $enrollments = $stmt->fetchAll();
     echo json_encode(['success' => true, 'data' => $enrollments]);
