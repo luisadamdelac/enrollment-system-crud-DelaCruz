be5de03efe6b2b7faee8de42485b70addb86def4
Dela Cruz, Luis Adam D. <luisadamdelacruz8@gmail.com>
2025-09-15 23:23:30 +0800

    Complete Student Management System

--- api/Students/getStudents.php
+++ api/Students/getStudents.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 require '../db.php';
 
 try {
-    $stmt = $pdo->query("SELECT s.stud_id, s.name, s.program_id, s.allowance, p.program_name FROM student_tbl s LEFT JOIN program_tbl p ON s.program_id = p.program_id");
+    $stmt = $pdo->query("SELECT s.stud_id, CONCAT(s.first_name, ' ', IF(s.middle_name != '', CONCAT(s.middle_name, ' '), ''), s.last_name) AS name, s.first_name, s.middle_name, s.last_name, s.program_id, s.allowance, p.program_name FROM student_tbl s LEFT JOIN program_tbl p ON s.program_id = p.program_id");
     $students = $stmt->fetchAll();
     echo json_encode(['success' => true, 'data' => $students]);
 } catch (Exception $e) {
